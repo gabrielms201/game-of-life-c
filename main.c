91c9446b7836b842c1674dbe3f646ffcccba03a2
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2022-05-30 23:47:56 -0300

    Prepara ambiente para o projeto

---
+++ main.c
@@ -0,0 +1,12 @@
+// | EP - JOGO DA VIDA John Conway's Game of Life      |
+// | Mayara Meneghetti Honda                  32152280 |
+// | Paulo Henrique Braga Cechinel            32151128 |
+// | Ricardo Gabriel Marques dos Santos Ruiz  32134908 |
+
+#include <stdio.h>
+
+int main( )
+{
+    printf("Hello, World!\n");
+    return 0;
+}
