908209d1cbe8b64d76cf375a605dc2160b316fbe
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2022-05-31 01:45:27 -0300

    Prepara geracao de matrix

---
+++ README.md
@@ -0,0 +1,8 @@
+# John Conway's Game Of Life
+
+## C Implementation
+
+### Detalhes sobre o projeto [aqui](https://ipbut.notion.site/EP-Jogo-da-Vida-c047c667b6f046f2ad2620627a8fa9f1)
+
+
+
--- main.c
+++ main.c
@@ -2,11 +2,41 @@
 // | Mayara Meneghetti Honda                  32152280 |
 // | Paulo Henrique Braga Cechinel            32151128 |
 // | Ricardo Gabriel Marques dos Santos Ruiz  32134908 |
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main( )
+
+//void generateMatrix(int **matrix, int lines, int columns);
+
+void *generateMatrix(int rows, int columns)
 {
-    printf("Hello, World!\n");
+    int (*matrix)[columns] = malloc (sizeof(int[rows][columns]));
+    if (matrix == NULL)
+    {
+        fprintf(stderr, "Falha ao tentar alocar memoria da matriz\n");
+        exit(-1);
+    }
+    return matrix;
+}
+void readRowsAndColumns(int* rows, int* columns)
+{
+    printf("Digite a quantidade de linhas para gerar a matriz: \n");
+    scanf("%d", &*rows);
+    printf("Digite a quantidade de colunas desejadas para gerar a matriz: \n");
+    scanf("%d", &*columns);
+}
+
+
+int main()
+{
+    int rows = 1;
+    int columns = 2;
+    //readRowsAndColumns(&rows, &columns);
+
+    int (*matrix)[columns] = generateMatrix(rows,columns);
+
+    printf("%d", matrix[0][0]);
+    free(matrix[rows]);
     return 0;
+
 }
