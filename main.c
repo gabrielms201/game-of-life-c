63d3abe53f7d456d096bfc4b0478e2e5bc444a4b
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2022-06-04 18:43:15 -0300

    Altera maneira de continuar geracao de matriz

--- main.c
+++ main.c
@@ -5,14 +5,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TEST_MODE
+// Descomentar esse define faz com que o programa ja crie uma matriz.
+// Fizemos isso para facilitar os testes durante a programacao
+//#define TEST_MODE
 const int g_ALIVE = 1;
 const int g_DEAD = 0;
 
+typedef struct
+{
+	int rows;
+	int columns;
+	int** data;
+} Matrix;
 
-int countLiveNearCells(int rowPosition, int columnPosition, int** matrix, int rows, int columns)
+
+// Funcao para contar a quantidade de vizinhos vivos dessa celula
+int countLiveNearCells(int rowPosition, int columnPosition, Matrix matrix)
 {
 	int i, j, count = 0;
+	// Abaixo fiz esse assing para evitar com que o numero de linhas ou colunas da matrix mude acidentalmente. ("encapsulamento")
+	int rows = matrix.rows;
+	int columns = matrix.columns;
+
 	for (i = rowPosition - 1; i <= rowPosition + 1; i++)
 	{
 		for (j = columnPosition - 1; j <= columnPosition + 1; j++)
@@ -21,7 +35,7 @@ int countLiveNearCells(int rowPosition, int columnPosition, int** matrix, int ro
 			{
 				continue;
 			}
-			if (matrix[i][j] == g_ALIVE)
+			if (matrix.data[i][j] == g_ALIVE)
 			{
 				count++;
 			}
@@ -30,31 +44,40 @@ int countLiveNearCells(int rowPosition, int columnPosition, int** matrix, int ro
 	return count;
 }
 
-void judgeCell(int rowPosition, int columnPosition, int** matrix, int rows, int columns, int** newMatrix)
+// Funcao para julgar uma celula a partir de quantos vizinhos ela tem e adicionar na nova matriz gerada
+void judgeCell(int rowPosition, int columnPosition, Matrix matrix, Matrix newMatrix)
 {
-	int nearLivingCells = countLiveNearCells(rowPosition, columnPosition, matrix, rows, columns);
+	// Primeiro devemos contar a quantidade de vizinhos vivos dessa celula:
+	int nearLivingCells = countLiveNearCells(rowPosition, columnPosition, matrix);
 
-	int* cell = &matrix[rowPosition][columnPosition];
+	// Defini um ponteiro para cada celula a fim de ter uma variavel ao inves de acessar toda hora (facilitando a codificacao)
+	int* cell = &matrix.data[rowPosition][columnPosition];
+	
+	// Motivos para a celula viver:
 	// Se tiver 2 ou 3 vizinhos, permanece viva
+	int livingCondition = *cell == g_ALIVE && (nearLivingCells == 2 || nearLivingCells == 3);
 	// Ou Se a celula estiver morta, mas com 3 vizinhos vivos, ela renasce
-	if (*cell == g_ALIVE && (nearLivingCells == 2 || nearLivingCells == 3) || (*cell == g_DEAD && nearLivingCells == 3))
+	int rebirthContition = *cell == g_DEAD && nearLivingCells == 3;
+	
+	if (livingCondition || rebirthContition)
 	{
-		newMatrix[rowPosition][columnPosition] = g_ALIVE;
+		newMatrix.data[rowPosition][columnPosition] = g_ALIVE;
 	}
-	// Se tiver 1 ou 0 vizinhos, morre de solidao ou se tiver 4 ou mais, morre de superpopulacao
-	/*else if (nearLivingCells == 1 || nearLivingCells == 0 || nearLivingCells >= 4)
-	{
-		*cell = g_DEAD;
-	}*/
+	// Motivos para a celula morrer:
+	// Se tiver 1 ou 0 vizinhos, morre de solidao
+	// Ou se tiver 4 ou mais vizinhos, morre de superpopulacao (podendo ser um else do primeiro)
 	else
 	{
-		newMatrix[rowPosition][columnPosition] = g_DEAD;
+		newMatrix.data[rowPosition][columnPosition] = g_DEAD;
 	}
 }
 
-
-void fillMatrix(int rows, int columns, int** matrix)
+// Funcao para preencher as celulas vivas de acordo com o input do usuario
+void fillAliveCellsByInput(Matrix matrix)
 {
+	// Abaixo fiz esse assing para evitar com que o numero de linhas ou colunas da matrix mude acidentalmente. ("encapsulamento")
+	int rows = matrix.rows;
+	int columns = matrix.columns;
 	printf("-> Informe as celulas vivas: \nOBS: Digite -1 na linha para terminar \n");
 	int i;
 	for (i = 0; i < rows * columns; i++)
@@ -62,28 +85,33 @@ void fillMatrix(int rows, int columns, int** matrix)
 		int row;
 		int col;
 		printf("\tLinha: ");
-		scanf("%d", &row);
+		scanf("%d%*c", &row);
 		if (row < 0) break;
 		printf("\tColuna: ");
-		scanf("%d", &col);
+		scanf("%d%*c", &col);
 		printf("\n");
-		matrix[row][col] = g_ALIVE;
+		matrix.data[row][col] = g_ALIVE;
 	}
 }
 
-void mockMatrix(int** matrix)
+// Funcao feita para facilitar os testes durante a codificacao. Ela pula a etapa de pedir pro usuario digitar as celulas vivas de uma matriz
+void mockMatrix(Matrix matrix)
 {
-	matrix[4][5] = g_ALIVE;
-	matrix[5][4] = g_ALIVE;
-	matrix[5][5] = g_ALIVE;
-	matrix[5][6] = g_ALIVE;
-	matrix[6][5] = g_ALIVE;
+	matrix.data[4][5] = g_ALIVE;
+	matrix.data[5][4] = g_ALIVE;
+	matrix.data[5][5] = g_ALIVE;
+	matrix.data[5][6] = g_ALIVE;
+	matrix.data[6][5] = g_ALIVE;
 }
 
-void printMatrix(int rows, int columns, int** matrix)
+// Funcao para imprimir uma grade + matriz
+void printMatrix(Matrix matrix)
 {
 	int i;
 	int j;
+	// Abaixo fiz esse assing para evitar com que o numero de linhas ou colunas da matrix mude acidentalmente. ("encapsulamento")
+	int rows = matrix.rows;
+	int columns = matrix.columns;
 
 	for (i = 0; i < rows; i++)
 	{
@@ -97,8 +125,8 @@ void printMatrix(int rows, int columns, int** matrix)
 		printf("|");
 		for (j = 0; j < columns; j++)
 		{
-			printf("%s|", matrix[i][j] == g_DEAD ? "   " : "<X>");
-			//printf("%d ", matrix[i][j]);
+			printf("%s|", matrix.data[i][j] == g_DEAD ? "   " : "<X>");
+			//printf("%d ", matrix.data[i][j]);
 		}
 		printf("\n");
 	}
@@ -110,32 +138,39 @@ void printMatrix(int rows, int columns, int** matrix)
 	printf("-\n");
 }
 
-int** generateMatrix(int rows, int columns)
+// Funcao para alocar os dados da matriz na heap e retornar uma struct "matrix"
+Matrix generateMatrix(int rows, int columns)
 {
 	int i;
-	int** matrix = (int**)calloc(rows, sizeof(int*));
+	int j;
+	Matrix matrix;
+	matrix.rows = rows;
+	matrix.columns = columns;
+	matrix.data = (int**)calloc(rows, sizeof(int*));
 	for (i = 0; i < rows; i++)
 	{
-		matrix[i] = (int*)calloc(columns, sizeof(int));
-		for (int j = 0; j < columns; j++)
+		matrix.data[i] = (int*)calloc(columns, sizeof(int));
+		for (j = 0; j < columns; j++)
 		{
-			matrix[i][j] = g_DEAD;
+			matrix.data[i][j] = g_DEAD;
 		}
 	}
 	return matrix;
 }
 
-void freeMatrix(int columns, int** matrix)
+// Funcao para liberar a matriz da heap
+void freeMatrix(Matrix matrix)
 {
 	int i;
-	for (i = 0; i < columns; i++)
+	for (i = 0; i < matrix.columns; i++)
 	{
-		free(matrix[i]);
-		matrix[i] = NULL;
+		free(matrix.data[i]);
+		matrix.data[i] = NULL;
 	}
-	free(matrix);
+	free(matrix.data);
 }
 
+// Funcao para ler o input do usuario e armazenar as linhas e colunas
 void readRowsAndColumns(int* rows, int* columns)
 {
 	printf("Digite a quantidade de linhas para gerar a matriz: \n");
@@ -143,8 +178,20 @@ void readRowsAndColumns(int* rows, int* columns)
 	printf("Digite a quantidade de colunas desejadas para gerar a matriz: \n");
 	scanf("%d", columns);
 }
-
-void gameOfLife()
+int shouldStop(int *end)
+{
+	char ch;
+	printf("Pressione apenas ENTER para continuar, ou digite qualquer coisa para parar \n");
+	scanf("%c", &ch);
+	if (ch == '0' || ch != '\n')
+	{
+		*end = 1;
+		return 1;
+	}
+	return 0;
+}
+// Funcao para iniciar o jogo da vida
+void startGameOfLife()
 {
 	int rows = 11;
 	int columns = 11;
@@ -152,48 +199,41 @@ void gameOfLife()
 
 #ifndef TEST_MODE
 	readRowsAndColumns(&rows, &columns);
-#endif
-	int** matrix = generateMatrix(rows, columns);
-
-#ifdef TEST_MODE
-	mockMatrix(matrix);
+	Matrix matrix = generateMatrix(rows, columns);
+	fillAliveCellsByInput(matrix);
 #else
-	fillMatrix(rows, columns, matrix);
+	Matrix matrix = generateMatrix(rows, columns);
+	mockMatrix(matrix);
 #endif
 
 	printf("Matriz Inicial: \n");
-	printMatrix(rows, columns, matrix);
+	printMatrix(matrix);
 	while (!end)
 	{
-		int i;
-		int j;
+		if(shouldStop(&end)) break;
 		printf("Proxima Matriz: \n");
-		int** newMatrix = generateMatrix(rows, columns);
+		Matrix newMatrix = generateMatrix(rows, columns);
+		// Agora sera gerado a nova matrix de acordo com as regras do jogo da vida
+		// Para cada celula, sera chamada a funcao que vai ver se ela deve ou nao morrer, e adicionar na nova matriz
+		int i, j;
 		for (i = 0; i < rows; i++)
 		{
 			for (j = 0; j < columns; j++)
 			{
-				judgeCell(i, j, matrix, rows, columns, newMatrix);
+				judgeCell(i, j, matrix, newMatrix);
 			}
 		}
-		printMatrix(rows, columns, newMatrix);
-		printf("Pressione qualquer tecla para continuar, ou digite (0) para parar: \n");
-		char choice[30];
-		scanf("%s*c", choice);
-		freeMatrix(columns, matrix);
+		printMatrix(newMatrix);
+		// Desaloca da heap a matriz antiga para evitar leaks, e aponta o ponteiro da antiga para a nova.
+		freeMatrix(matrix);
 		matrix = newMatrix;
-		if (strcmp(choice, "0") == 0)
-		{
-			end = 1;
-		}
-		getchar();
-		
 	}
-	freeMatrix(columns, matrix);
+	// Limpa a matriz da heap
+	freeMatrix(matrix);
 }
 
 int main()
 {
-	gameOfLife();
+	startGameOfLife();
 	return 0;
 }
